a423d4a07219fcb529455e7ab8a6301d4c1bce60
Aude Rossignol <arossignol@lucca.fr>
2019-10-09 15:00:05 +0000

    Ajout du fichier de listing

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,32 @@
 activities = [
+  {"Name"=>"Trampoline",
+  "Category"=>"Sport",
+  "City"=>"Nantes",
+  "Duration"=>60,
+  "Number of participants"=>10,
+  "Price"=>120},
+  {"Name"=>"Flechette",
+  "Category"=>"Agilité",
+  "City"=>"Nantes",
+  "Duration"=>50,
+  "Number of participants"=>10,
+  "Price"=>100},
+  {"Name"=>"Degustation de vins",
+  "Category"=>"Détente",
+  "City"=>"Clisson",
+  "Duration"=>120,
+  "Number of participants"=>10,
+  "Price"=>220}
   # TODO: gather at least 3 activities
 ]
+
+activities.each do |activity|
+  puts "#{activity["Name"]} #{activity["Category"].upcase}"
+  puts "Located in #{activity["City"]}"
+  puts "Duration of #{activity["Duration"]} minutes"
+  puts "For #{activity["Number of participants"]} personnes"
+  puts "Charge of #{activity["Price"]} €"
+  puts "---"
+end
 
 # TODO: your code goes here

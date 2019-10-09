cb4675be91f3caa54a12ec8cc24b6c4c74dbd412
Aude Rossignol <arossignol@lucca.fr>
2019-10-09 14:40:59 +0000

    Ajout du fichier des détails

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,11 @@
-activity = {
-  # TODO: gather the characteristics of your preferred activity
-}
+activity = {"name"=>"Trampoline","Category"=>"Sport","City"=>"Nantes","Duration"=>60,"Number of participants"=>10,"Price"=>120}
+puts "#{activity["name"]} #{activity["Category"].upcase}"
+puts "Located in #{activity["City"]}"
+puts "Duration of #{activity["Duration"]} minutes"
+puts "For #{activity["Number of participants"]} personnes"
+puts "Charge of #{activity["Price"]} €"
 
-# TODO: your code goes here
+  # TODO: gather the characteristics of your preferred activity
+
+
+

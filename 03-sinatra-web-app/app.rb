62da609ca4ffac1d81224fb882578541e69e18a9
Aude Rossignol <arossignol@lucca.fr>
2019-10-09 16:40:40 +0000

    Sinatra et modif exo git basic

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,8 +6,24 @@ enable :static
 
 get "/" do
   @activities = [
-    # TODO: gather a few activities
+  {"Name"=>"Trampoline",
+  "Category"=>"Sport",
+  "City"=>"Nantes",
+  "Duration"=>60,
+  "Number of participants"=>10,
+  "Price"=>120},
+  {"Name"=>"Flechette",
+  "Category"=>"Agilité",
+  "City"=>"Nantes",
+  "Duration"=>50,
+  "Number of participants"=>10,
+  "Price"=>100},
+  {"Name"=>"Degustation de vins",
+  "Category"=>"Détente",
+  "City"=>"Clisson",
+  "Duration"=>120,
+  "Number of participants"=>10,
+  "Price"=>220}
   ]
-
   erb :index
 end

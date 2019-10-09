9d36f7f92033b7da902a310568b63004e4b93cf4
Aude Rossignol <arossignol@lucca.fr>
2019-10-09 14:01:42 +0000

    Ajout du programme des catagories

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,21 @@
-categories = [
+categories = ["Adventure","Sport","Visits","Workshops","Cultural","Food"]
   # TODO: gather the categories of activity
-]
+puts "Please, ddd an extra category..."
+category = gets.chomp # asks user to type something in the Terminal
+
+categories << category
+
+categories = categories.sort
+
+categories.each do |name|
+  puts "#{name}"
+end
+
+puts categories.size
 
 # TODO: your code goes here
+#1. Create an `Array` of categories containing at least:
+#Adventure, Sport, Visits, Workshops, Cultural, Food (in this **specific order**)
+#2. Ask the user of your program to enter an extra category
+#3. Display the list of the categories sorted alphabetically
+#4. Display the total number of categories

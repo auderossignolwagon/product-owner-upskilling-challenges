31826a9dbb1ebb876c084cc02ec6be2c09a27454
Aude Rossignol <arossignol@lucca.fr>
2019-10-11 14:12:31 +0000

    Ajout programme lister une utilisateur

--- 05-api-consumption/01-ruby-program/search_user.rb
+++ 05-api-consumption/01-ruby-program/search_user.rb
@@ -6,4 +6,19 @@ name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for that person..."
 
-# TODO: your code goes here
+namefirstname = name.split()
+
+# header = {"Authorization"=>"Lucca application=18afcfb8-697b-4742-b726-418269485cee"}
+
+url = "https://wagon.ilucca-demo.net/api/v3/users"
+
+response = RestClient.get(url, "params" => {"fields"=>"firstName,lastName,department,picture","firstname"=>namefirstname[0],"lastName"=>namefirstname[1]}, "Authorization" => "Lucca application=18afcfb8-697b-4742-b726-418269485cee")
+personne = JSON.parse(response.body)
+
+p "------------"
+p personne["data"]["items"][0]["firstName"]
+p personne["data"]["items"][0]["lastName"]
+p personne["data"]["items"][0]["department"]["name"]
+p personne["data"]["items"][0]["picture"]["url"]
+p "------------"
+
